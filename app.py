0f90bbb5aef63c9a5aedcf5ddceb1cefd91bad01
Nesma Nasser <nesmanasser162@gmail.com>
2025-08-14 00:34:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-تحميل الموديل
+#تحميل الموديل
 @st.cache_resource
 def load_model():
     model = tf.keras.models.load_model("skin_model.h5", compile=False)
@@ -11,13 +11,13 @@ def load_model():
 
 model = load_model()
 
-ترويسة
+#ترويسة
 st.title("Skin Cancer Classification")
 
-رفع صورة
+#رفع صورة
 uploaded_file = st.file_uploader("Upload a skin lesion image", type=["jpg", "jpeg", "png"])
 
-لو في صورة مرفوعة
+#لو في صورة مرفوعة
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption='Uploaded Image', use_column_width=True)
